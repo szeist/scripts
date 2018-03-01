087c3c171aff9d60849e82382d77af4cc1fcd162
Istvan Szenasi <szeist@gmail.com>
2018-03-01 08:49:30 +0100

    set rate in fix_monitors.sh

--- fix_monitors.sh
+++ fix_monitors.sh
@@ -9,10 +9,10 @@ xrandr --output DP-2-1 --off
 xrandr --output DP-2-2 --off;
 
 if [ $DISPLAYPORT_COUNT -eq 2 ]; then
-    xrandr --output DP-2-1 --above eDP-1 --mode 1920x1080
-    xrandr --output DP-2-2 --above eDP-1 --left-of DP-2-1 --mode 1920x1080;
+    xrandr --output DP-2-1 --above eDP-1 --mode 1920x1080 -r 60
+    xrandr --output DP-2-2 --above eDP-1 --left-of DP-2-1 --mode 1920x1080 -r 60
 fi
 
 if [ $HDMI_COUNT -eq 1 ]; then
-    xrandr --output HDMI-2 --above eDP-1 --mode 1920x1080
+    xrandr --output HDMI-2 --above eDP-1 --mode 1920x1080 -r 60
 fi
