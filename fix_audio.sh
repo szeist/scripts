8caa2fd9ec3eddac5f1c0314ddceac5d3a834f9a
Istvan Szenasi <szeist@gmail.com>
2020-04-12 16:43:22 +0200

    remove fix_audio.sh

--- fix_audio.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-killall -9 pulseaudio
-rm -r ~/.config/pulse/*      
-sudo alsa force-reload                                                                                          
-
