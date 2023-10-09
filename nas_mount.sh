a95df0a403c9959b384814e2868047237676265e
Istvan Szenasi <szeist@gmail.com>
2023-10-09 07:58:36 +0200

    update nas mount protocol version

--- nas_mount.sh
+++ nas_mount.sh
@@ -12,5 +12,5 @@ else
   [ -d ${MOUNT_POINT} ] || sudo mkdir -p ${MOUNT_POINT}
 
   NAS_PASS=$(bw --raw get password $NAS_BW_SECRET_ID)
-  sudo mount -t cifs -o username=${NAS_USER},password=${NAS_PASS},file_mode=0600,dir_mode=0700,vers=1.0,uid=$(id -u),gid=$(id -g) //${NAS_IP}/${NAS_SHARE} ${MOUNT_POINT};
+  sudo mount -t cifs -o username=${NAS_USER},password=${NAS_PASS},file_mode=0600,dir_mode=0700,vers=3.0,uid=$(id -u),gid=$(id -g) //${NAS_IP}/${NAS_SHARE} ${MOUNT_POINT};
 fi
